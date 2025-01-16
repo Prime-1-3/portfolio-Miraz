e287fc252fb043137bf6ae556a8d4680f561d335
Miraz <skmirazrahman21@gmail.com>
2025-01-16 23:21:17 +0600

    Add About section and 404 error page; integrate Bootstrap and Lottie animations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavbarComponent from './components/navbar/Nav';
 import FooterComponent from './components/footer/Footer';
 import Home from './page/Home/HomePage';
+import About from './components/about/aboutme/About';
+// import EducationJourney from './components/about/milestone/EducationJourney';
+// import ExperienceJourney from './components/about/milestone/ExperienceJourney';
+// import TechnicalJourney from './components/about/milestone/TechnicalJourney';
+// import PersonalSkill from './components/about/skills/PersonalSkill';
+// import TechnicalSkill from './components/about/skills/TechnicalSkill';
+import Err404 from './page/notfound';
 
 function App() {
   return (
@@ -13,7 +20,15 @@ function App() {
       <div className="App">
         <NavbarComponent />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />}>
+            <Route index element={<About />} />
+            {/* <Route path="personal-skill" element={<PersonalSkill />} />
+            <Route path="technical-skill" element={<TechnicalSkill />} />
+            <Route path="technical-journey" element={<TechnicalJourney />} />
+            <Route path="education-journey" element={<EducationJourney />} />
+            <Route path="experience-journey" element={<ExperienceJourney />} /> */}
+          </Route>
+          <Route path="*" element={<Err404 />} />
         </Routes>
         <FooterComponent />
       </div>
--- src/page/Home/HomePage.js
+++ src/page/Home/HomePage.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../../Particle";
 import Typing from "./Typing";
-// import AboutmeContainer from "../../components/aboutme/aboutmeContainer";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn, FaFacebookF, FaGoogle } from "react-icons/fa";
-// import Certification from "../../components/certificate/Certification";
-// import Projects from "../../components/projects/Projects";
-// import Contactus from "../../components/contactus/Contactus";
+import InfoContainer from "../../components/about/InfoContainer";
 
 export default function Home() {
+  const socialLinks = [
+  {href: 'https://github.com/Prime-1-3', icon: <AiFillGithub /> },
+  {href: 'https://www.linkedin.com/in/sk-miraz-rahman-ani-00ab6a326/', icon: <FaLinkedinIn /> },
+  {href: 'https://www.instagram.com/sk_miraz_rahman/', icon: <AiFillInstagram /> },
+  {href: 'https://www.facebook.com/Sk.Miraz.FB.profile', icon: <FaFacebookF /> },
+  {href: 'https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=skmirazrahman18@gmail.com', icon: <FaGoogle /> },
+  ];
   return (
     <section className="home-section">
       <Container fluid id="home">
@@ -36,76 +40,32 @@ export default function Home() {
                 <Typing />
               </div>
               <div className="sociallink-home">
+
                 <ul className="homeaboutsociallinks">
-                  <li className="socialicons">
-                    <a
-                      href="https://github.com/Prime-1-3"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="iconcolour  homesocialicons"
-                    >
-                      <AiFillGithub />
-                    </a>
-                  </li>
-                  <li className="socialicons">
-                    <a
-                      href="https://www.linkedin.com/in/sk-miraz-rahman-ani-00ab6a326/"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="iconcolour  homesocialicons"
-                    >
-                      <FaLinkedinIn />
-                    </a>
-                  </li>
-                  <li className="socialicons">
-                    <a
-                      href="https://www.instagram.com/sk_miraz_rahman/"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="iconcolour homesocialicons"
-                    >
-                      <AiFillInstagram />
-                    </a>
-                  </li>
-                  <li className="socialicons">
-                    <a
-                      href="https://www.facebook.com/Sk.Miraz.FB.profile"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="iconcolour homesocialicons"
-                    >
-                      <FaFacebookF />
-                    </a>
-                  </li>
-                  <li className="socialicons">
-                    <a
-                      href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=skmirazrahman18@gmail.com"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="iconcolour homesocialicons"
-                    >
-                      <FaGoogle />
-                    </a>
-                  </li>
+                  {socialLinks.map((link, index) => (
+                    <li className="socialicons" key={index}>
+                      <a href={link.href} target="_blank" rel="noreferrer" className="iconcolour homesocialicons">
+                        {link.icon}
+                      </a>
+                    </li>
+                  ))}
                 </ul>
+
               </div>
             </Col>
             <Col md={4} style={{ paddingBottom: 20 }}>
-            <div className="img-home-main">
-                    <img
-                      src="./profile.jpg"
-                      alt="home pic"
-                      className="img-fluid"
-                    />
-                  </div>
+              <div className="img-home-main">
+                <img
+                  src="./profile.jpg"
+                  alt="home pic"
+                  className="img-fluid"
+                />
+              </div>
             </Col>
           </Row>
         </Container>
       </Container>
-      {/* <AboutmeContainer />
-      <Certification />
-      <Projects />
-      <Contactus /> */}
+      <InfoContainer />
     </section>
   );
 }
